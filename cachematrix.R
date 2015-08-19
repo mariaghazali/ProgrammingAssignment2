1db8d1a85518a28edb1cf358dbdb9dd0bfc975ad
mariaghazali <maria.ghazali@gmail.com>
2015-08-19 14:27:10 +0300

    this is the result for Programm Assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Calculates the Inverse of a Matrix.
+## Takes the matrix from cache if available.
+## Matrix must be square and invertable.
 
-## Write a short comment describing this function
-
+## step 1. makeCacheMatrix function creates a special "matrix" object 
+##      that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+        m<-NULL
+        set<-function(y){
+                x<<-y # search through the parents environments for the definition of x
+                m<<-NULL # search through the parents environments for the definition of m
+        }
+        get<-function() x
+        setsolve<-function(solve) m<<-solve
+        getsolve<-function() m
+        list(set=set, get=get,
+             setsolve=setsolve, getsolve=getsolve)
 }
 
+## step 2. cacheSolve function uses object (x) created with makeCacheMatrix and 
+##      returns inverse of the cached matrix. 
 
-## Write a short comment describing this function
+##      This function computes the inverse of the special "matrix" returned 
+##      by makeCacheMatrix above. If the inverse has already been
+##      calculated (and the matrix has not changed), then cacheSolve
+##      should retrieve the inverse from the cache.
+
+##      Check for the squareness of the matrix will do solve function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # ... - for additional args from solve
+        m<-x$getsolve()
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+        }
+        data<-x$get()
+        m<-solve(data, ...)
+        x$setsolve(m)
+        m
 }
+
+# # usage
+# matr<-matrix(rnorm(25),5,5)
+# x<-makeCacheMatrix(matr)
+# cache.solve<-cacheSolve(x)
+# cache.solve
+# solve(matr)==cache.solve
